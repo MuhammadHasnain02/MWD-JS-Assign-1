934166ce2926d0eeb2325311fa469e00d50157cd
muhammadhasnain02 <mrhasnain9009@gmail.com>
2025-02-25 22:06:07 -0800

    Assignents Files

--- Chapter 2/script.js
+++ Chapter 2/script.js
@@ -1,54 +1,56 @@
+var myName = "Jhone Doe"
+var myAge  = "15 years old"
+var myData = "Certified Mobile Application Development"
+var word   = "PIZZA"
+var email  = "example@example.com"
+var book   = "“A smarter way to learn JavaScript"
+var end    = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
 
-let text_1 = "Error! Please enter a valid password"
-alert(text_1)
 
-let text_2 = "Welcome to JS Land... \n Happy Coding!"
-alert(text_2)
+alerts("")
+alerts("")
+alerts("")
+alerts("")
+alerts("")
+alerts("")
+alerts("▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬")
 
-let text_3 = "Welcome to JS Land..."
-alert(text_3)
 
-let text_4 = "Happy Coding!"
-alert(text_4)
+// let myName = "Jhone Doe"
+// let text_6 = myName
+// alert(text_6)
 
-let text_5 = "Hello... I can run JS through my web browser's console"
-alert(text_5)
+// let myAge = "15 years old"
+// let text_7 = myAge
+// alert(text_7)
 
-let myName = "Jhone Doe"
-let text_6 = myName
-alert(text_6)
+// let qualification = "Certified Mobile Application Development" 
+// let text_8 = qualification
+// alert(text_8)
 
-let myAge = "15 years old"
-let text_7 = myAge
-alert(text_7)
+// let text_9 = "PIZZA \n PIZZ \n PIZ \n PI \n P"
+// alert(text_9)
 
-let qualification = "Certified Mobile Application Development" 
-let text_8 = qualification
-alert(text_8)
+// let email = "example@example.com"
+// let text_10 = "My email adress is " + email
+// alert(text_10)
 
-let text_9 = "PIZZA \n PIZZ \n PIZ \n PI \n P"
-alert(text_9)
+// let bookName = "A smarter way to learn JavaScript"
+// let text_11 = "I am trying to learn from the book \n"+ bookName
+// alert(text_11)
 
-let email = "example@example.com"
-let text_10 = "My email adress is " + email
-alert(text_10)
+// let age = 15
+// let text_12 = "I am "+ age + " years old"
+// alert(text_12)
 
-let bookName = "A smarter way to learn JavaScript"
-let text_11 = "I am trying to learn from the book \n"+ bookName
-alert(text_11)
+// let times = 14
+// let text_13 = "You have visited this site "+ times +" times"
+// alert(text_13)
 
-let age = 15
-let text_12 = "I am "+ age + " years old"
-alert(text_12)
+// let birthYear = 1990
+// let text_14 = "My birth year is "+birthYear
+// alert(text_14)
 
-let times = 14
-let text_13 = "You have visited this site "+ times +" times"
-alert(text_13)
-
-let birthYear = 1990
-let text_14 = "My birth year is "+birthYear
-alert(text_14)
-
-let quantity = 5
-let text_15 = "John Doe ordered "+ quantity +" T-shirt(s) on ABC Clothing store"
-alert(text_15)
+// let quantity = 5
+// let text_15 = "John Doe ordered "+ quantity +" T-shirt(s) on ABC Clothing store"
+// alert(text_15)
